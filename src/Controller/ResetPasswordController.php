f8e7ec25f2da985bd2d2be4415ff320f21b7c64b
Niels Scheunemann <niels.scheunemann@bluewin.ch>
2021-04-19 18:49:34 +0200

    Typo

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -155,7 +155,7 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address($this->getParameter('sender_mail');, $this->getParameter('appname')))
+            ->from(new Address($this->getParameter('sender_mail'), $this->getParameter('appname')))
             ->to($user->getEmail())
             ->subject('Your password reset request')
             ->htmlTemplate('reset_password/email.html.twig')
