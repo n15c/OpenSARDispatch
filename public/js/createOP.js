90b0c72c7d643b386a99af63818d1d9a1898e686
Niels Scheunemann <niels.scheunemann@bluewin.ch>
2021-04-19 18:40:47 +0200

    Fixed Mapbox Token public -> Needs to be improved (config value)

--- public/js/createOP.js
+++ public/js/createOP.js
@@ -24,6 +24,6 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibmlzYyIsImEiOiJja2s0M2Q0dXcxaDU0MnZycmhrYzRkcmUxIn0.3iM2lfCBX3qa_UNdcRmusg'
+    accessToken: 'pk.eyJ1IjoibmlzYyIsImEiOiJja25vdHE0a24wZHI4MnVudzdvMDN6eTR6In0.-wdVL8FXmLmjfum4Vok66A'
 }).addTo(mymap);
 mymap.on('click', onMapClick);
