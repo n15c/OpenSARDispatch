f3aaa980d63a5fcdcc36b394e1d2daf91529dce6
Niels Scheunemann <niels.scheunemann@bluewin.ch>
2021-04-19 18:38:05 +0200

    Added Mail Fix to PW Reset sender

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -155,7 +155,7 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address('app@n3xus.io', $this->getParameter('appname')))
+            ->from(new Address($this->getParameter('sender_mail');, $this->getParameter('appname')))
             ->to($user->getEmail())
             ->subject('Your password reset request')
             ->htmlTemplate('reset_password/email.html.twig')
